f214699b67d7929806729fd3ffe3a44ed111d93f
Genny <gennarocecco122@gmail.com>
2023-11-25 15:37:10 +0100

    development completed for sentiment analysis webapp

--- static/script.js
+++ static/script.js
@@ -15,6 +15,11 @@ function inviaDati() {
             success: function (response) {
                 console.log(response)
                 $('#corpoRNN').append('<h1 id="risultatoTitolo">Risultato</h1>');
+                $('#corpoRNN').css({
+                    'margin-top': '20rem',
+                    'text-align': 'center'
+                });
+                $('#risultatoTitolo').css('margin-top', '20px');
                 $('#risultatoTitolo').append('<p>' + response + '</p>');
                 $('#corpoRNN').append('<button id="restart">Restart</button>');
                 $('#restart').on('click', function () {
@@ -35,3 +40,81 @@ function inviaDati() {
         });
     }
 }
+
+//funzione che fa apparire il footer allo scroll del mouse
+document.addEventListener("DOMContentLoaded", function () {
+    var footer = document.querySelector("footer");
+    var corpoRNN = document.getElementById("corpoRNN");
+
+    // Nascondi il footer all'inizio
+    footer.style.opacity = "0";
+    footer.style.visibility = "hidden";
+
+    window.addEventListener("load", function () {
+        // Quando la pagina e tutte le risorse sono caricate
+        var showFooterHeight = corpoRNN.offsetTop + corpoRNN.offsetHeight - window.innerHeight;
+
+        // Aggiorna l'opacità e la visibilità in base alla posizione di scorrimento
+        function updateFooterVisibility() {
+            var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+            if (scrollPosition > showFooterHeight) {
+                footer.style.opacity = "1";
+                footer.style.visibility = "visible";
+            } else {
+                footer.style.opacity = "0";
+                footer.style.visibility = "hidden";
+            }
+        }
+
+        // Aggiorna la visibilità del footer quando si verifica lo scroll
+        window.addEventListener("scroll", updateFooterVisibility);
+
+        // Chiamata iniziale per gestire lo stato all'inizio
+        updateFooterVisibility();
+    });
+});
+
+
+// funzione che mostra o nasconde la freccia durante lo scrolling
+window.onscroll = function () {
+    showScrollToTop();
+};
+
+function showScrollToTop() {
+    var scrollToTop = document.getElementById("scrollToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTop.style.display = "block";
+    } else {
+        scrollToTop.style.display = "none";
+    }
+}
+
+// Torna all'inizio con transizione più lenta quando clicchi sulla freccia
+document.getElementById("scrollToTop").addEventListener("click", function () {
+    scrollToTopWithTransition();
+});
+
+function scrollToTopWithTransition() {
+    var scrollOptions = {
+        top: 0,
+        behavior: "smooth",
+    };
+
+    window.scrollTo(scrollOptions);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
